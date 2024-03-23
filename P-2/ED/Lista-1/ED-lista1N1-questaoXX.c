d08d0100bf7cc1beba79d0646b9e94b5b04510e5
Perazzojoao <perazzojoao@gmail.com>
2024-03-23 16:45:20 -0300

    Adiciona modelo de arquivo

---
+++ P-2/ED/Lista-1/ED-lista1N1-questaoXX.c
@@ -0,0 +1,17 @@
+/*
+** Função :
+** Autor : João Victor Barbosa Perazzo
+** Data : 23/03/2024
+** Observações:
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+  setlocale(LC_ALL, "Portugese");
+
+
+
+  return 0;
+}
